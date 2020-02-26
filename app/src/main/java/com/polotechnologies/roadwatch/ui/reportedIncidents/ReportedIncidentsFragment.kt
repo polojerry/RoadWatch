22b715aa36617eabed1c19d769f52e5eb310ce9e
polojerry <polojerry4@gmail.com>
2020-02-26 10:02:22 +0300

    Created Reported Incident Fragment, completed design

---
+++ app/src/main/java/com/polotechnologies/roadwatch/ui/reportedIncidents/ReportedIncidentsFragment.kt
@@ -0,0 +1,23 @@
+package com.polotechnologies.roadwatch.ui.reportedIncidents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.polotechnologies.roadwatch.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ReportedIncidentsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reported_incidents, container, false)
+    }
+
+}
