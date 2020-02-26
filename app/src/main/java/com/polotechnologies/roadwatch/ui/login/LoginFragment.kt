65ad17935d8277a41ca5135d24568c9b8a81bb3b
polojerry <polojerry4@gmail.com>
2020-02-26 11:16:14 +0300

    Updated Admin Login

--- app/src/main/java/com/polotechnologies/roadwatch/ui/login/LoginFragment.kt
+++ app/src/main/java/com/polotechnologies/roadwatch/ui/login/LoginFragment.kt
@@ -10,7 +10,9 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.polotechnologies.roadwatch.R
+import com.polotechnologies.roadwatch.dataModels.User
 import com.polotechnologies.roadwatch.databinding.FragmentLoginBinding
 
 /**
@@ -20,6 +22,7 @@ class LoginFragment : Fragment() {
 
     private lateinit var mBinding: FragmentLoginBinding
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: FirebaseFirestore
 
     private var userEmail = ""
     private var userPassword = ""
@@ -31,15 +34,30 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseFirestore.getInstance()
 
         checkLoginStatus()
+
         initiateClickListeners()
         return mBinding.root
     }
 
     private fun checkLoginStatus() {
         if(mAuth.currentUser != null){
-            findNavController().navigate(R.id.action_loginFragment_to_roadIncidentsFragment)
+            mDatabase.collection("users")
+                .whereEqualTo("user_id", mAuth.currentUser!!.uid)
+                .get()
+                .addOnSuccessListener {documentSnapshot->
+                    val user  : MutableList<User> = documentSnapshot.toObjects(User::class.java)
+                    val userData : User = user[0]
+
+                    if(userData.account_type == "admin"){
+                        findNavController().navigate(R.id.action_loginFragment_to_reportedIncidentsFragment)
+                    }else{
+                        findNavController().navigate(R.id.action_loginFragment_to_roadIncidentsFragment)
+                    }
+
+                }
         }
     }
 
@@ -91,8 +109,7 @@ class LoginFragment : Fragment() {
             mBinding.progressLogin.visibility = View.GONE
 
             if (it.isSuccessful) {
-                Toast.makeText(context, "Login Successful...", Toast.LENGTH_SHORT).show()
-                findNavController().navigate(R.id.action_loginFragment_to_roadIncidentsFragment)
+                checkLoginStatus()
             }
         }.addOnFailureListener {
             Toast.makeText(context, "Failed to Login..." + it.message, Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/polotechnologies/roadwatch/ui/reportedIncidents/ReportedIncidentsFragment.kt
+++ app/src/main/java/com/polotechnologies/roadwatch/ui/reportedIncidents/ReportedIncidentsFragment.kt
@@ -3,21 +3,53 @@ package com.polotechnologies.roadwatch.ui.reportedIncidents
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.polotechnologies.roadwatch.R
+import com.polotechnologies.roadwatch.databinding.FragmentReportedIncidentsBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class ReportedIncidentsFragment : Fragment() {
+class ReportedIncidentsFragment : Fragment(), Toolbar.OnMenuItemClickListener {
 
+    private lateinit var mBinding : FragmentReportedIncidentsBinding
+    private lateinit var mAuth:FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reported_incidents, container, false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_reported_incidents, container, false)
+        mAuth = FirebaseAuth.getInstance()
+
+        inflateMenu()
+
+        return mBinding.root
+    }
+
+    private fun inflateMenu() {
+        mBinding.toolbarReportedRoadIncidents.inflateMenu(R.menu.menu_main)
+        mBinding.toolbarReportedRoadIncidents.setOnMenuItemClickListener(this)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.action_sign_out -> signOut()
+        }
+        return true
+    }
+
+    private fun signOut() {
+        mAuth.signOut()
+        Toast.makeText(context, "Logged Out Successfully", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_reportedIncidentsFragment_to_loginFragment)
     }
 
 }
